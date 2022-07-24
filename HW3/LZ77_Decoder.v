e9a333b30d39374cc2f43335645341ed2a9b6966
LinJ0 <n96104137@gs.ncku.edu.tw>
2022-07-24 13:31:21 +0800

    Update LZ77_Decoder.v 69 errors

--- HW3/LZ77_Decoder.v
+++ HW3/LZ77_Decoder.v
@@ -9,30 +9,31 @@ output  			finish;
 output 	 	[7:0] 	char_nxt;
 
 reg [7:0] char_nxt;
-reg [7:0] s_buf [9:0];
+reg [7:0] s_buf [8:0];
 reg [2:0] stall;
 wire encode, finish;
 
-//output reg
+//output logic
 always@(*) begin
-    if(reset) char_nxt = 8'h0; 
-	else char_nxt = s_buf[0];
+	char_nxt = s_buf[0];
 end
-
+//stall counter
 always@(posedge clk or posedge reset) begin
     if(reset) stall <= 3'b0; 
 	else begin 
 		stall <= (stall)? (stall - 1) : code_len;
 	end
 end
-
-//s_buf shift
+//s_buf control
 integer i;
 always@(posedge clk or posedge reset) begin
-	for(i=0; i<9; i=i+1) begin
-		s_buf[i + 1] <= s_buf[i];
+	if(reset) s_buf[0] <= 8'b0; 
+	else begin
+		for(i=0; i<8; i=i+1) begin
+			s_buf[i + 1] <= s_buf[i];
+		end
+	s_buf[0] <= ((stall[0] && code_len) || !code_len)? chardata : s_buf[code_pos];
 	end
-	s_buf[0] <= (stall)? s_buf[code_pos] : chardata;
 end
 
 //finish signal control
@@ -41,4 +42,3 @@ assign finish = (char_nxt == 8'h24)? 1 : 0;
 //encode signal control
 assign encode = 0;
 endmodule
-
